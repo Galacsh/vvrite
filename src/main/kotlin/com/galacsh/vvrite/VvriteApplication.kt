a90e0daa533d928636715555299d6ed03d5512bf
Galacsh <galacsh.dev@gmail.com>
2025-03-19 16:55:55 +0900

    project init

---
+++ src/main/kotlin/com/galacsh/vvrite/VvriteApplication.kt
@@ -0,0 +1,11 @@
+package com.galacsh.vvrite
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VvriteApplication
+
+fun main(args: Array<String>) {
+    runApplication<VvriteApplication>(*args)
+}
---
+++ src/test/kotlin/com/galacsh/vvrite/VvriteApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.galacsh.vvrite
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VvriteApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
